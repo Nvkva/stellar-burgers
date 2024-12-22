1b8bba789b83a47e1b18324a1376dfcf9392050b
Diana Novikova <bogdanova.4.diana@yandex.ru>
2024-12-22 14:58:11 +0300

    Починила тесты cypress

--- cypress/e2e/constructor/constructor.cy.ts
+++ cypress/e2e/constructor/constructor.cy.ts
@@ -1,16 +1,22 @@
 describe('Burger Builder Integration Test', () => {
   beforeEach(() => {
     // Загрузка приложения
-    cy.visit('http://192.168.1.137:4000/');
+    cy.visit('http://192.168.1.137:4000/login');
+
+    cy.get('input[name=email]').type('kit.solt@mail.ru')
+    cy.get('input[name=password]').type(`${'qwerty'}{enter}`, { log: false })
   });
 
   it('should allow adding ingredients, placing an order, and resetting the constructor', () => {
+
     // Проверяем, что ингредиенты отображаются на странице
     cy.get('[data-test-id="ingredient"]').should('have.length.at.least', 1);
 
     // Добавляем булку в конструктор
     cy.get('[data-test-id="ingredient"]')
-      .contains('Булка') // Ищем элемент с текстом "Булка"
+      .contains('булка') // Ищем элемент с текстом "Булка"
+      .first()
+      .parent()
       .as('bun');
     cy.get('@bun').find('button').click();
 
@@ -20,7 +26,7 @@ describe('Burger Builder Integration Test', () => {
 
     // Добавляем ингредиент (например, начинку)
     cy.get('[data-test-id="ingredient"]')
-      .not(':contains("Булка")') // Пропускаем булку
+      .not(':contains("булка")') // Пропускаем булку
       .first()
       .as('ingredient');
     cy.get('@ingredient').find('button').click();
@@ -31,32 +37,77 @@ describe('Burger Builder Integration Test', () => {
     // Проверяем отображение цены
     cy.get('[data-test-id="total-price"]').then(($price) => {
       const price = parseInt($price.text(), 10);
-      expect(price).toBeGreaterThan(0);
+      expect(price).greaterThan(0);
     });
 
     // Имитируем успешный запрос на оформление заказа
     cy.intercept('POST', '/api/orders', {
       statusCode: 200,
-      body: { number: 12345 },
+      body: {
+        success: true,
+        name: "Краторный люминесцентный бургер",
+        order: {
+          ingredients: [
+            {
+              _id: "643d69a5c3f7b9001cfa093c",
+              name: "Краторная булка N-200i",
+              type: "bun",
+              proteins: 80,
+              fat: 24,
+              carbohydrates: 53,
+              calories: 420,
+              price: 1255,
+              image: "https://code.s3.yandex.net/react/code/bun-02.png",
+              image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+              image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+              __v: 0
+            },
+            {
+              _id: "643d69a5c3f7b9001cfa093e",
+              name: "Филе Люминесцентного тетраодонтимформа",
+              type: "main",
+              proteins: 44,
+              fat: 26,
+              carbohydrates: 85,
+              calories: 643,
+              price: 988,
+              image: "https://code.s3.yandex.net/react/code/meat-03.png",
+              image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+              image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+              __v: 0
+            }
+          ],
+          _id: "67672d89750864001d3736ef",
+          owner: {
+            name: "Жак-Ив Кусто Жак-Ив",
+            email: "kit.solt@mail.ru",
+            createdAt: "2024-11-30T19:40:48.751Z",
+            updatedAt: "2024-12-03T17:47:36.991Z"
+          },
+          status: "done",
+          name: "Краторный люминесцентный бургер",
+          createdAt: "2024-12-21T21:05:13.411Z",
+          updatedAt: "2024-12-21T21:05:14.244Z",
+          number: 63730,
+          price: 2243
+        }
+      },
     }).as('placeOrder');
 
     // Клик по кнопке «Оформить заказ»
     cy.get('[data-test-id="place-order-button"]').click();
 
     // Проверяем, что модальное окно открылось
-    cy.get('[data-test-id="order-modal"]').should('be.visible');
+    cy.get('[data-test-id="modal"]').should('be.visible');
 
     // Проверяем, что номер заказа корректен
-    cy.get('[data-test-id="order-number"]').should('contain.text', '12345');
+    cy.get('[data-test-id="order-number"]').should('contain.text', '63730');
 
     // Закрываем модальное окно
     cy.get('[data-test-id="modal-close-button"]').click();
 
     // Проверяем, что модальное окно закрыто
-    cy.get('[data-test-id="order-modal"]').should('not.exist');
-
-    // Проверяем, что конструктор пуст
-    cy.get('[data-test-id="constructor-item"]').should('not.exist');
+    cy.get('[data-test-id="modal"]').should('not.exist');
 
     // Проверяем, что отображается сообщение о необходимости выбора булок
     cy.get('[data-test-id="constructor-empty"]').should('contain.text', 'Выберите булки');
--- cypress/e2e/dailog/ingredient-dailog.cy.ts
+++ cypress/e2e/dailog/ingredient-dailog.cy.ts
@@ -1,48 +1,55 @@
 describe('Ingredient Modal', () => {
   beforeEach(() => {
     // Открываем главную страницу перед каждым тестом
-    cy.visit('http://192.168.1.137:4000/');
+    cy.visit('http://192.168.1.137:4000/login');
 
     // Дожидаемся загрузки данных об ингредиентах
     cy.intercept('GET', '**/ingredients').as('getIngredients');
     cy.wait('@getIngredients');
+
+    cy.get('input[name=email]').type('kit.solt@mail.ru')
+    cy.get('input[name=password]').type(`${'qwerty'}{enter}`, { log: false })
+
+    
   });
 
   it('should open the ingredient modal and display correct details', () => {
     // Ищем первый ингредиент и кликаем на него
-    cy.get('[data-testid="ingredient"]').first().as('firstIngredient');
+    cy.get('[data-test-id="ingredient"]').first().as('firstIngredient');
     cy.get('@firstIngredient').click();
 
     // Проверяем, что модальное окно открылось
-    cy.get('[data-testid="modal"]').should('be.visible');
+    cy.get('[data-test-id="modal"]').should('be.visible');
 
     // Проверяем, что отображаются правильные данные ингредиента
-    cy.get('@firstIngredient').within(() => {
-      cy.get('[data-testid="ingredient-name"]').then(($name) => {
-        const ingredientName = $name.text();
-        cy.get('[data-testid="modal-title"]').should('contain', ingredientName);
+    cy.get('[data-test-id="ingredient-detail-name"]').as('ingredientDetailName').then(() => {
+      cy.get('@firstIngredient').within(() => {
+        cy.get('[data-test-id="ingredient-card-name"]').then(($name) => {
+          const ingredientName = $name.text();
+          cy.get('@ingredientDetailName').should('contain', ingredientName);
+        });
       });
-    });
+    })
 
     // Закрываем модальное окно
-    cy.get('[data-testid="modal-close-button"]').click();
+    cy.get('[data-test-id="modal-close-button"]').click();
 
     // Убеждаемся, что модальное окно закрылось
-    cy.get('[data-testid="modal"]').should('not.exist');
+    cy.get('[data-test-id="modal"]').should('not.exist');
   });
 
   it('should close the modal when close button is clicked', () => {
     // Ищем первый ингредиент и кликаем на него
-    cy.get('[data-testid="ingredient"]').first().as('firstIngredient');
+    cy.get('[data-test-id="ingredient"]').first().as('firstIngredient');
     cy.get('@firstIngredient').click();
 
     // Проверяем, что модальное окно открылось
-    cy.get('[data-testid="modal"]').should('be.visible');
+    cy.get('[data-test-id="modal"]').should('be.visible');
 
     // Кликаем на кнопку закрытия
-    cy.get('[data-testid="modal-close-button"]').click();
+    cy.get('[data-test-id="modal-close-button"]').click();
 
     // Проверяем, что модальное окно закрылось
-    cy.get('[data-testid="modal"]').should('not.exist');
+    cy.get('[data-test-id="modal"]').should('not.exist');
   });
 });
